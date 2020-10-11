d957f4a95dbee6747b018dda46788367063bd05e
mag2679 <68792606+mag2679@users.noreply.github.com>
2020-10-11 14:20:03 -0700

    add mailer function

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+import axios from 'axios'
+window.axios = axios
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
@@ -16,5 +18,5 @@ ReactDOM.render(
   document.querySelector("#root")
 );
 
-console.log("STRIPE KEY IS", process.env.REACT_APP_STRIPE_KEY);
-console.log("STRIPE KEY IS", process.env.NODE_ENV);
+// console.log("STRIPE KEY IS", process.env.REACT_APP_STRIPE_KEY);
+// console.log("STRIPE KEY IS", process.env.NODE_ENV);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 // const authRoutes = require("./routes/authRoutes");
 
@@ -26,6 +27,7 @@ app.use(passport.session());
 // authRoutes(app);
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   //serve up producyion assets
