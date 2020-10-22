96adc2b6c0ea7778f3c5c2b7e9237199d891983e
mag2679 <68792606+mag2679@users.noreply.github.com>
2020-10-22 15:40:38 -0700

    survey results

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,7 +12,7 @@
 
 // Method 2
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get("/api/current_user");
@@ -31,4 +31,10 @@ export const submitSurvey = (values, history) => async dispatch => {
   
   history.push('/surveys')
   dispatch({type:FETCH_USER, payload:res.data})
+}
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys')
+
+  dispatch({type:FETCH_SURVEYS, payload: res.data})
 }
